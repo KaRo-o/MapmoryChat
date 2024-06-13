43c0b8699442a4732f1cca44efefa390e77d3561
KaRo <nghngh1008@gmail.com>
2024-06-14 00:28:16 +0900

    1

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ function App() {
   return (
     <div>
       <Routes>
+        <Route path="/chatting/chatlist" element={<ChatList />} />
+        <Route path="/chatting/chatroom/:chat_room_id" element={<Chat />} />
+        <Route path="chatting/chatlist" element={<ChatList />} />
+        <Route path="chatting/chatroom/:chat_room_id" element={<Chat />} />
+        <Route path="/chatlist" element={<ChatList />} />
+        <Route path="/chatroom/:chat_room_id" element={<Chat />} />
         <Route path="chatlist" element={<ChatList />} />
         <Route path="chatroom/:chat_room_id" element={<Chat />} />
       </Routes>
