766ddeeeb58e9510332318199f74b9d6cc197012
KaRo <nghngh1008@gmail.com>
2024-06-20 21:55:15 +0900

    1

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ const ioo = new Server(server, {
   },
 });
 
-// const socket = io("https://mapmory.co.kr");
-const socket = io("http://192.168.0.45:3001");
+const socket = io("https://mapmory.co.kr");
+// const socket = io("http://192.168.0.45:3001");
 
 app.use(cors());
 app.use(express.json());
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, "build")));
   try {
     const response = await axios.get(
       // "http://192.168.0.45:8000/chat/json/getMongo"
-      "http://192.168.0.45:8000/chat/json/getMongo"
+      "https:mapmory.co.kr/chat/json/getMongo"
     );
     console.log(
       "=======================================================",
